02885abfd39d8b51701d1ebfde6b39c2b6cdc0f1
Marceli Machlowski <marceli.machlowski@gmial.com>
2023-03-14 14:38:16 +0100

    First commit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
+    public function index($name): JsonResponse
+    {
+        return $this->json([
+            'message' => 'My movie@: '.$name.'!',
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+}
